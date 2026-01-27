b401930275b1d34fe6f5578cc1072728cab37eb2
AnDi24 <setwe@mail.ru>
2026-01-27 13:08:22 +1000

    удален неиспользуемый импорт в NavGraph (исправлена опечатка в названии класса

--- app/src/main/java/ru/practicum/android/diploma/ui/compose/NavGraph.kt
+++ app/src/main/java/ru/practicum/android/diploma/ui/compose/NavGraph.kt
@@ -18,7 +18,6 @@ import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import androidx.navigation.NavGraph.Companion.findStartDestination
 import androidx.navigation.compose.NavHost
