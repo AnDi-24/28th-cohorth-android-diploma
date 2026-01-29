d70b608c4ab65e20377eb4aa9d7768845b12fbec
AnDi24 <setwe@mail.ru>
2026-01-29 21:06:50 +1000

    Добавлено поле с поисковым запросом

--- app/src/main/java/ru/practicum/android/diploma/ui/compose/MainScreen.kt
+++ app/src/main/java/ru/practicum/android/diploma/ui/compose/MainScreen.kt
@@ -1,6 +1,5 @@
 package ru.practicum.android.diploma.ui.compose
 
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
@@ -9,8 +8,10 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import ru.practicum.android.diploma.R
 
 @Composable
 fun MainScreen(navController: NavController) {
@@ -18,11 +19,9 @@ fun MainScreen(navController: NavController) {
         modifier = Modifier
             .fillMaxSize()
             .padding(16.dp),
-        verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-
-        Text(text = "Экран 1 — Главная")
+        SearchField(stringResource(R.string.request_placeholder))
         Button(onClick = {
             navController.navigate(VACANCY)
         }) {
---
+++ app/src/main/java/ru/practicum/android/diploma/ui/compose/SearchField.kt
@@ -0,0 +1,71 @@
+package ru.practicum.android.diploma.ui.compose
+
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import ru.practicum.android.diploma.R
+
+@Composable
+fun SearchField(label: String) {
+    var query by rememberSaveable { mutableStateOf("") }
+    var inputValue by remember { mutableStateOf(query) }
+
+    TextField(
+        value = inputValue,
+        onValueChange = { newValue ->
+            inputValue = newValue
+            query = newValue},
+        shape = RoundedCornerShape(12.dp),
+        modifier = Modifier
+            .height(70.dp)
+            .fillMaxWidth()
+            .padding(vertical = 8.dp),
+        placeholder = { Text(label)},
+        trailingIcon = {
+            if (inputValue.isNotEmpty()) {
+                IconButton(
+                    onClick = {
+                        query = ""
+                        inputValue = ""
+//                            viewModel.clearButton()
+                    }
+                ) {
+                    Icon(
+                        painter = painterResource(id = R.drawable.ic_close),
+                        tint = colorResource(R.color.black_universal),
+                        contentDescription = null
+                    )
+                }
+            }else{
+                Icon(
+                    painter = painterResource(id = R.drawable.ic_search),
+                    tint = colorResource(R.color.black_universal),
+                    contentDescription = null
+                )
+            }
+        },
+        colors = TextFieldDefaults.colors(
+            focusedContainerColor = colorResource(R.color.light_grey),
+            unfocusedContainerColor = colorResource(R.color.light_grey),
+            focusedIndicatorColor = Color.Transparent,
+            unfocusedIndicatorColor = Color.Transparent
+        )
+    )
+}
